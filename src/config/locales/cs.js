771650b35cddaceab258d62d409ea2f4701e0734
Hybernia <113475061+hybernia1@users.noreply.github.com>
2026-01-19 09:08:57 +0100

    Split localization files by language

---
+++ src/config/locales/cs.js
@@ -0,0 +1,10 @@
+export const cs = {
+  loadingTitle: "Tiled",
+  pressAnyKey: "Stiskni libovolnou klávesu pro pokračování",
+  menuTitle: "Hlavní menu",
+  menuStart: "Spustit hru",
+  menuLanguage: "Jazyk",
+  languageName: "Čeština",
+  demoInstructions:
+    "WASD/šipky: pohyb | Mezerník: střelba | E: interakce | B: inventář | Dotyk: ovládací tlačítka",
+};
--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import * as Phaser from "phaser";
 import { createGameConfig } from "./config/gameConfig.js";
 import { DemoScene } from "./scenes/DemoScene.js";
+import { LoadingScene } from "./scenes/LoadingScene.js";
+import { MenuScene } from "./scenes/MenuScene.js";
 
-const config = createGameConfig(DemoScene);
+const config = createGameConfig([LoadingScene, MenuScene, DemoScene]);
 
 new Phaser.Game(config);
